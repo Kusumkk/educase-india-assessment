7ea2deb8891c61856724aab7c4b2f0d54406d787
Kusumkk <147970313+Kusumkk@users.noreply.github.com>
2026-06-25 12:05:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Welcome to PopX</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet,
+          <br />
+          consectetur adipiscing elit.
+        </p>
+
+        <button className="create-btn">Create Account</button>
+
+        <button className="login-btn">
+          Already Registered? Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
